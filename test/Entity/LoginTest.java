7208d5dd5feeece5532dd7dfad71462d5d111063
Ibbless <q412113590@hotmail.com>
2015-09-23 02:58:00 +0800

    1
    1

---
+++ test/Entity/LoginTest.java
@@ -0,0 +1,22 @@
+package Entity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.junit.Test;
+
+public class LoginTest {
+
+	@Test
+	public void test() {
+		Configuration cfg = new Configuration().configure();
+		ServiceRegistry regestry = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
+		SessionFactory sf = cfg.buildSessionFactory(regestry);
+		Session session = sf.getCurrentSession();
+		SchemaExport export = new SchemaExport(cfg);
+		
+		export.create(true, true);
+	}
+}
